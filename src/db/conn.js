5ff18394c14a2b16afec7dc5316ffc7fe2605e99
Srimathy Kumar <113321552+srimathy-kumar@users.noreply.github.com>
2023-05-01 23:53:32 +0530

    file uploads

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://srimathymec:toc8AvxZmWliMx22@cluster0.g3lrbti.mongodb.net/InternProject3?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((e)=>{
+    console.log(`no connection`);
+});
